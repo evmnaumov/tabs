ccf74be705ce0b623c743f297e6df16e71c19229
Jery <jery@MacBook-Pro-cmadmin-2.local>
2018-03-13 23:36:50 +0200

    sadf

--- info.php
+++ info.php
@@ -8,13 +8,9 @@ $tabs_db = mysqli_select_db($link,'tabs');
 // get the result object.
 //$data=array();
 $result = mysqli_query($link,'SELECT * FROM `test`');
-//while($row = mysqli_fetch_array($result, MYSQLI_NUM)){
-// fetch the result row.
-//;
-//$data[] = $row;
 foreach($result as $key=>$value){
     foreach($value as $key=>$val){
-    echo "$val<br>";}
+    echo $val["title"]. "<br>";}
 }       //["image"] .", " . $data["title"] .", " . $data["price"] . ", " . $data["description"] . "<br>";
 mysqli_free_result($result);
 //}
