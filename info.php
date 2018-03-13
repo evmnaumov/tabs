c640b50f521c6f51a69054acfcb241e3048a67c6
Jery <jery@MacBook-Pro-cmadmin-2.local>
2018-03-13 23:34:13 +0200

    asdfasdfasdf

--- info.php
+++ info.php
@@ -6,17 +6,17 @@ if (mysqli_connect_errno()) {
 }
 $tabs_db = mysqli_select_db($link,'tabs');
 // get the result object.
-$data=array();
+//$data=array();
 $result = mysqli_query($link,'SELECT * FROM `test`');
-while($row = mysqli_fetch_array($result, MYSQLI_NUM)){
+//while($row = mysqli_fetch_array($result, MYSQLI_NUM)){
 // fetch the result row.
 //;
-$data[] = $row;
-foreach($data as $key=>$value){
+//$data[] = $row;
+foreach($result as $key=>$value){
     foreach($value as $key=>$val){
     echo "$val<br>";}
 }       //["image"] .", " . $data["title"] .", " . $data["price"] . ", " . $data["description"] . "<br>";
 mysqli_free_result($result);
-}
+//}
 mysqli_close($link);
 ?>
