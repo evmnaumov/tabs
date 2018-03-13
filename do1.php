2ee7760ccf3e14b4af7d5f9b8cc69838546dacc0
Jery <jery@MacBook-Pro-cmadmin-2.local>
2018-03-13 14:43:17 +0200

    do1.php

--- do1.php
+++ do1.php
@@ -1,6 +1,6 @@
 <?php
 	// Соединяемся, выбираем базу данных
-$link = mysql_connect('127.0.0.1', 'root', 'rebOOt365')
+$link = mysql_connect('127.0.0.1', 'root', 'rebOOt365');
 if (!$link) {
     die('Ошибка соединения: ' . mysql_error());
 }
