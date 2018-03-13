38fd4cd479c2635f5d3c13d56ae444389919c111
Jery <jery@MacBook-Pro-cmadmin-2.local>
2018-03-13 15:33:28 +0200

    db

--- do1.php
+++ do1.php
@@ -4,7 +4,7 @@ $host="localhost";
 $user="tabs";
 $pass="rebOOt365";
 
-$link = mysql_connect('$host', '$user', '$pass');
+$link = mysql_connect($host, $user, $pass);
 if (!$link) {
     die('Ошибка соединения: ' . mysql_error());
 }
