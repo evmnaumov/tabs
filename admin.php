3d82ed72acf1dca8fe11c970698c5af37d49d76d
Jery <jery@MacBook-Pro-cmadmin-2.local>
2018-03-13 23:40:09 +0200

    admin test

--- admin.php
+++ admin.php
@@ -9,27 +9,26 @@
 <?php
 //$xml = simplexml_load_file("db.xml");
 
-    // Соединяемся, выбираем базу данных
-
-    $link = mysqli_connect('localhost', 'tabs', 'rebOOt365');
-    if (mysqli_connect_errno()) {
-        printf("Connect failed: %s\n", mysqli_connect_error());
-        exit();
-    }
-    $tabs_db = mysqli_select_db($link,'tabs');
-    
+// Соединяемся, выбираем базу данных
+$link = mysqli_connect('localhost', 'tabs', 'rebOOt365');
+if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
+}
+$tabs_db = mysqli_select_db($link,'tabs');
+$result = mysqli_query($link,'SELECT * FROM `test`');
 
 $i=0;
-foreach ($xml->item as $item) {
+foreach ($result as $item) {
     global $i;
     $a="";
     if($i<1){
         $a="showing";
     };
-        ?><input type='text' name='txt[<?php echo $i;?>][image]' value='img/<?php echo $item->image;?>'>
-          <input type='text' name='txt[<?php echo $i;?>][title]' value='<?php echo $item->title;?>'>
-          <input type='text' name='txt[<?php echo $i;?>][price]' value='<?php echo $item->price;?>'>
-          <input type='text' name='txt[<?php echo $i;?>][description]' value='<?php echo $item->description;?>'>
+        ?><input type='text' name='txt[<?php echo $i;?>][image]' value='img/<?php echo $item["image"];?>'>
+          <input type='text' name='txt[<?php echo $i;?>][title]' value='<?php echo $item["title"];?>'>
+          <input type='text' name='txt[<?php echo $i;?>][price]' value='<?php echo $item["price"];?>'>
+          <input type='text' name='txt[<?php echo $i;?>][description]' value='<?php echo $item["description"];?>'>
 		  <br>
     <?php
     $i=$i+1;
