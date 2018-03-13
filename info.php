b01ba556a5f7d88c13b8689887b0470aa413d77c
Jery <jery@MacBook-Pro-cmadmin-2.local>
2018-03-13 23:29:33 +0200

    dfasdfas

--- info.php
+++ info.php
@@ -1,21 +1,21 @@
 <?php
-$link = mysqli_connect('localhost', 'tabs', 'rebOOt365');
-if (mysqli_connect_errno()) {
-    printf("Connect failed: %s\n", mysqli_connect_error());
-    exit();
-}
-$tabs_db = mysqli_select_db($link,'tabs');
+$link = mysql_connect('localhost', 'tabs', 'rebOOt365');
+//if (mysql_connect_errno()) {
+//    printf("Connect failed: %s\n", mysqli_connect_error());
+//    exit();
+//}
+$tabs_db = mysql_select_db('tabs');
 // get the result object.
 $data=array();
-$result = mysqli_query($link,'SELECT * FROM `test`');
-while($row = mysqli_fetch_array($result, MYSQLI_NUM)){
+$result = mysql_query('SELECT * FROM `test`');
+while($row = mysql_fetch_array($result)){
 // fetch the result row.
 //;
 $data[] = $row;
 foreach($data as $value){
     echo "$value <br>";
 }       //["image"] .", " . $data["title"] .", " . $data["price"] . ", " . $data["description"] . "<br>";
-mysqli_free_result($result);
+mysql_free_result($result);
 }
-mysqli_close($link);
+mysql_close($link);
 ?>
