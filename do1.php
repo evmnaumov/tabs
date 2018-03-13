47e6f54be2418cc858858b386428ff0aefabd172
Jery <jery@MacBook-Pro-cmadmin-2.local>
2018-03-13 15:48:53 +0200

    db

--- do1.php
+++ do1.php
@@ -1,10 +1,7 @@
 <?php
     // Соединяемся, выбираем базу данных
-//$host="localhost";
-//$user="tabs";
-//$pass="rebOOt365";
 
-$link = mysql_connect('localhost', 'tabs', 'rebOOt365');
+$link = mysqli_connect('localhost', 'tabs', 'rebOOt365');
 if (!$link) {
     die('Ошибка соединения: ' . mysql_error());
 }
