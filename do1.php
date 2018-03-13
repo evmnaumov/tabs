7c2f8b1e14151b02062d0b31cc8d5121d16e867a
Jery <jery@MacBook-Pro-cmadmin-2.local>
2018-03-13 16:40:59 +0200

    db

--- do1.php
+++ do1.php
@@ -3,8 +3,53 @@
 
 $link = mysqli_connect('localhost', 'tabs', 'rebOOt365');
 if (!$link) {
-    die('Ошибка соединения: ' . mysql_error());
+    die('Ошибка соединения: ' . mysqli_error());
 }
 echo 'Успешно соединились';
-mysql_close($link);
+
+mysqli_select_db('tabs') or die('Не удалось выбрать базу данных');
+
+
+//if (isset($_POST['txt'])){
+ 
+    foreach($_POST['txt'] as $value){
+        foreach($value as $key=>$val){
+            echo "$key, $val";
+        }
+
+
+    }
+/*
+    // Переменные с формы
+    $name = $_POST['name'];
+    $text = $_POST['text'];
+     
+    // Параметры для подключения
+    $db_host = "localhost"; 
+    $db_user = "user"; // Логин БД
+    $db_password = "123"; // Пароль БД
+    $db_table = "mytable"; // Имя Таблицы БД
+     
+    // Подключение к базе данных
+    $db = mysql_connect($db_host,$db_user,$db_password) OR DIE("Не могу создать соединение ");
+     
+    // Выборка базы
+    mysql_select_db("mydb",$db);
+     
+    // Установка кодировки соединения
+    mysql_query("SET NAMES 'utf8'",$db);
+     
+    $result = mysql_query ("INSERT INTO ".$db_table." (name,text) VALUES ('$name','$text')");
+     
+    if ($result = 'true'){
+        echo "Информация занесена в базу данных";
+    }else{
+        echo "Информация не занесена в базу данных";
+    }
+}
+*/
+
+
+
+mysqli_close($link);
 ?>
