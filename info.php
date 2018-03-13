06fe215380009717b4c18fb529e31599f8dd7938
Jery <jery@MacBook-Pro-cmadmin-2.local>
2018-03-13 23:30:41 +0200

    asdf

--- info.php
+++ info.php
@@ -1,21 +1,21 @@
 <?php
-$link = mysql_connect('localhost', 'tabs', 'rebOOt365');
-//if (mysql_connect_errno()) {
-//    printf("Connect failed: %s\n", mysqli_connect_error());
-//    exit();
-//}
-$tabs_db = mysql_select_db('tabs');
+$link = mysqli_connect('localhost', 'tabs', 'rebOOt365');
+if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
+}
+$tabs_db = mysqli_select_db($link,'tabs');
 // get the result object.
 $data=array();
-$result = mysql_query('SELECT * FROM `test`');
-while($row = mysql_fetch_array($result)){
+$result = mysqli_query($link,'SELECT * FROM `test`');
+while($row = mysqli_fetch_array($result, MYSQLI_NUM)){
 // fetch the result row.
 //;
 $data[] = $row;
 foreach($data as $value){
-    echo "$value <br>";
+    echo "$value<br>";
 }       //["image"] .", " . $data["title"] .", " . $data["price"] . ", " . $data["description"] . "<br>";
-mysql_free_result($result);
+mysqli_free_result($result);
 }
-mysql_close($link);
+mysqli_close($link);
 ?>
