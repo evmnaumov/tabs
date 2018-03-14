05ff9db054876e98cb61e03f9132086981873531
Jery <jery@MacBook-Pro-cmadmin-2.local>
2018-03-14 16:54:39 +0200

    front

--- index.php
+++ index.php
@@ -8,18 +8,25 @@
 <div id="content">
 <ul id="slides">
 <?php
-$xml = simplexml_load_file("db.xml");
+// Соединяемся, выбираем базу данных
+$link = mysqli_connect('localhost', 'tabs', 'rebOOt365');
+if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
+}
+$tabs_db = mysqli_select_db($link,'tabs');
+$result = mysqli_query($link,'SELECT * FROM `test`');
 $i=0;
-foreach ($xml->item as $item) {
+foreach ($result as $item) {
     global $i;
     $a="";
     if($i<1){
         $a="showing";
     };
-        ?><li class="slide <?php echo $a;?>" style="background-image: url(img/<?php echo $item->image;?>);">
-            <p class="title"><?php echo $item->title;?></p>
-            <p class="price"><?php echo $item->price;?></p>
-            <p class="description"><?php echo $item->description;?></p>
+        ?><li class="slide <?php echo $a;?>" style="background-image: url(img/<?php echo $result["image"];?>);">
+            <p class="title"><?php echo $result["title"];?></p>
+            <p class="price"><?php echo $result["price"];?></p>
+            <p class="description"><?php echo $result["description"];?></p>
         </li>
     <?php
     $i=$i+1;
